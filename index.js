b75ee96bfbde62f373f39c8e6a397ec6abf0b6ec
Sam <ssamsanya3@gmail.com>
2025-03-12 07:35:56 +0300

    newme

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+if (mainElement) {
+  mainElement.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Sam is the champion"; 
+
+document.body.appendChild(newHeader);
